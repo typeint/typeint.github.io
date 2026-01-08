776099482d885a2b2bfe68cf011da1a50d7c6a76
typeint <typeinteger@proton.me>
2026-01-08 15:36:34 +0000

    Create blog.sh

---
+++ blog.sh
@@ -0,0 +1,14 @@
+rm -rf index.md web
+mkdir web
+for file in *.md; do
+    date=$(echo "$file" | cut -d- -f1-3)
+    path=$(echo "$file" | cut -d- -f4- | cut -d. -f1)
+    title=$(head -1 "$file" | cut -d" " -f2-)
+    echo $date
+    echo $path
+    mkdir "web/$path"
+    set -x; pandoc "$file" --template "template.html" -V "ispost=ispost" -V "title=$title" -V "who=Jim T" -V "date=$date" -o "web/$path/index.html"; set +x
+    echo -e "$date - <a href=\"$path/\">$title</a>\n" >> index.md
+done
+set -x; pandoc "index.md" --template "template.html" -V "who=Jim T" -o "web/index.html"; set +x
+rm -f index.md
