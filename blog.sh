f484f6026f4770ec2f7284da86869845d74ef816
typeint <typeinteger@proton.me>
2026-01-08 17:27:05 +0000

    Update blog.sh

--- blog.sh
+++ blog.sh
@@ -12,5 +12,13 @@ for file in posts/*.md; do
     echo "$date - <a href=\"$path/\">$title</a>\n" >> index.md
     echo >> index.md
 done
+for file in others/*.md; do
+    basename=$(basename "$file")
+    path=$basename
+    title=$(head -1 "$file" | cut -d" " -f2-)
+    echo $path
+    mkdir "web/$path"
+    set -x; pandoc "$file" --template "template.html" -V "ispost=ispost" -V "title=$title" -V "who=Jim T" -o "web/$path/index.html"; set +x
+done
 set -x; pandoc "index.md" --template "template.html" -V "who=Jim T" -o "web/index.html"; set +x
 rm -f index.md
